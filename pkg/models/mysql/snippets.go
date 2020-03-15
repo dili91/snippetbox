9f4cf15938e61e9c36e141d5c6385d96b9c90b69
andreadilisio <andrea.dilisio@onebip.com>
2020-03-15 19:35:56 +0100

    implemeted insert

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -13,7 +13,22 @@ type SnippetModel struct {
 
 // Insert : This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int type
+	// before returning.
+	return int(id), nil
 }
 
 // Get : This will return a specific snippet based on its id.
